f6b973ce856f53b51778ba8086a0ad6bcf7d0763
ShuaiHao <e1221805@u.nus.edu>
2025-08-30 21:19:34 +0800

    Try to fix network

--- CTAI_web/vue.config.js
+++ CTAI_web/vue.config.js
@@ -9,6 +9,9 @@ module.exports = {
   css: undefined,
   devServer: {
     port: 7500,
-    host: '0.0.0.0' // 添加这一行
+    host: '0.0.0.0',
+    allowedHosts: 'all', // 允许所有主机访问
+    // 或使用下面这个（旧版 vue-cli）
+    // disableHostCheck: true
   }
 }
